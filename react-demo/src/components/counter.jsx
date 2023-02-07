68b86be23dfad082240b3eaf8cfdd83481c48418
git.assignment-1 <cesskui4034@gmail.com>
2023-02-07 13:44:01 +0300

    props

---
+++ react-demo/src/components/counter.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+export class  Counter extends React.Component {
+ constructor(props) {
+    super(props);
+    this.state={ count: 0}
+    this.handleIncrement.handleIncrement.bind(this)
+    this.handleDecrement.handleDecrement.bind(this)
+}
+ handleIncrement() {
+  //this.state({count:10});
+  this.setState((prev)=>{return{count:prev.count-1}
+})
+
+ }
+
+ handleDecrement() {
+  //this.setState({count: 0});
+  this.setState((prev)=>{
+    return {count: prev.count +1}
+});
+
+ }
+
+
+        render() {
+        return(
+            <div className = 'counter'>
+                <span className ='numbers'>{this.state.count}</span>
+                <span className ='button'>
+                    <button onClick={this.handleIncrement}> + </button>
+                    <button onClick={this.handleDecrement} > - </button>
+                </span>
+
+            </div>
+        )
+    }
+}
+
+
+export default Counter;
